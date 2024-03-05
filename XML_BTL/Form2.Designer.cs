8ba21b97349ea43bc48f0ccf5f6c63e8d30cf134
Admin <Admin@DESKTOP-D3VGUUB>
2024-03-05 17:08:39 +0700

    huy

---
+++ XML_BTL/Form2.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace XML_BTL
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTk = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lbMk = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTk
+            // 
+            this.lbTk.AutoSize = true;
+            this.lbTk.Location = new System.Drawing.Point(279, 196);
+            this.lbTk.Name = "lbTk";
+            this.lbTk.Size = new System.Drawing.Size(55, 13);
+            this.lbTk.TabIndex = 0;
+            this.lbTk.Text = "Username";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(340, 193);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(340, 234);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 3;
+            // 
+            // lbMk
+            // 
+            this.lbMk.AutoSize = true;
+            this.lbMk.Location = new System.Drawing.Point(279, 237);
+            this.lbMk.Name = "lbMk";
+            this.lbMk.Size = new System.Drawing.Size(53, 13);
+            this.lbMk.TabIndex = 2;
+            this.lbMk.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Đăng Nhập";
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Location = new System.Drawing.Point(340, 281);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(75, 23);
+            this.btn_dangnhap.TabIndex = 5;
+            this.btn_dangnhap.Text = "Đăng nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lbMk);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbTk);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTk;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label lbMk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_dangnhap;
+    }
+}
