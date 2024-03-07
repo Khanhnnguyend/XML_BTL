aecb48987c3f2e9d74a81cbf5857b79fa779325f
Quangion <quang.itv.vtc@gmail.com>
2024-03-07 20:45:53 +0700

    Quang dep trai 07/03 8h45'

--- XML_BTL/Form1.Designer.cs
+++ XML_BTL/Form1.Designer.cs
@@ -32,10 +32,37 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cl2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTK3 = new System.Windows.Forms.TextBox();
+            this.btTK3 = new System.Windows.Forms.Button();
+            this.rdoTH3 = new System.Windows.Forms.RadioButton();
+            this.rdoMH3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSLT3 = new System.Windows.Forms.TextBox();
+            this.txtDonGia = new System.Windows.Forms.TextBox();
+            this.txtSL3 = new System.Windows.Forms.TextBox();
+            this.txtTenSP3 = new System.Windows.Forms.TextBox();
+            this.txtMaSP3 = new System.Windows.Forms.TextBox();
+            this.btXong3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -54,7 +81,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(837, 450);
+            this.tabControl1.Size = new System.Drawing.Size(1177, 720);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -67,10 +94,9 @@
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.tabPage1.Size = new System.Drawing.Size(829, 418);
+            this.tabPage1.Size = new System.Drawing.Size(1169, 688);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Nhập hóa đơn";
-         
             // 
             // tabPage2
             // 
@@ -79,19 +105,240 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 28);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(829, 418);
+            this.tabPage2.Size = new System.Drawing.Size(1169, 688);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Quản lý sản phẩm";
             // 
             // tabPage3
             // 
             this.tabPage3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tabPage3.Controls.Add(this.dgv);
+            this.tabPage3.Controls.Add(this.groupBox2);
+            this.tabPage3.Controls.Add(this.groupBox1);
             this.tabPage3.Location = new System.Drawing.Point(4, 28);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(829, 418);
+            this.tabPage3.Size = new System.Drawing.Size(1169, 688);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Quản lý nhập hàng";
+            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.cl2,
+            this.Column2,
+            this.Column3});
+            this.dgv.Location = new System.Drawing.Point(156, 368);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(842, 286);
+            this.dgv.TabIndex = 2;
+            this.dgv.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dgv_MouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "maSP";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // cl2
+            // 
+            this.cl2.HeaderText = "tên SP";
+            this.cl2.MinimumWidth = 6;
+            this.cl2.Name = "cl2";
+            this.cl2.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Số Lượng";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "đơn giá ";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.txtTK3);
+            this.groupBox2.Controls.Add(this.btTK3);
+            this.groupBox2.Controls.Add(this.rdoTH3);
+            this.groupBox2.Controls.Add(this.rdoMH3);
+            this.groupBox2.Location = new System.Drawing.Point(664, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 255);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "tìm kiếm ";
+            // 
+            // txtTK3
+            // 
+            this.txtTK3.Location = new System.Drawing.Point(54, 82);
+            this.txtTK3.Name = "txtTK3";
+            this.txtTK3.Size = new System.Drawing.Size(280, 22);
+            this.txtTK3.TabIndex = 3;
+            // 
+            // btTK3
+            // 
+            this.btTK3.Location = new System.Drawing.Point(148, 189);
+            this.btTK3.Name = "btTK3";
+            this.btTK3.Size = new System.Drawing.Size(97, 39);
+            this.btTK3.TabIndex = 2;
+            this.btTK3.Text = "tìm kiếm ";
+            this.btTK3.UseVisualStyleBackColor = true;
+            this.btTK3.Click += new System.EventHandler(this.btTK3_Click);
+            // 
+            // rdoTH3
+            // 
+            this.rdoTH3.AutoSize = true;
+            this.rdoTH3.Location = new System.Drawing.Point(247, 145);
+            this.rdoTH3.Name = "rdoTH3";
+            this.rdoTH3.Size = new System.Drawing.Size(87, 20);
+            this.rdoTH3.TabIndex = 1;
+            this.rdoTH3.TabStop = true;
+            this.rdoTH3.Text = "tên hàng";
+            this.rdoTH3.UseVisualStyleBackColor = true;
+            // 
+            // rdoMH3
+            // 
+            this.rdoMH3.AutoSize = true;
+            this.rdoMH3.Location = new System.Drawing.Point(67, 145);
+            this.rdoMH3.Name = "rdoMH3";
+            this.rdoMH3.Size = new System.Drawing.Size(87, 20);
+            this.rdoMH3.TabIndex = 0;
+            this.rdoMH3.TabStop = true;
+            this.rdoMH3.Text = "Mã hàng";
+            this.rdoMH3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtSLT3);
+            this.groupBox1.Controls.Add(this.txtDonGia);
+            this.groupBox1.Controls.Add(this.txtSL3);
+            this.groupBox1.Controls.Add(this.txtTenSP3);
+            this.groupBox1.Controls.Add(this.txtMaSP3);
+            this.groupBox1.Controls.Add(this.btXong3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(29, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(532, 255);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "thêm sản phẩm vào kho ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(174, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "số lượng sản phẩm thêm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(263, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "đơn giá ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Số lượng ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "tên SP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "mã SP";
+            // 
+            // txtSLT3
+            // 
+            this.txtSLT3.Location = new System.Drawing.Point(205, 210);
+            this.txtSLT3.Name = "txtSLT3";
+            this.txtSLT3.Size = new System.Drawing.Size(90, 22);
+            this.txtSLT3.TabIndex = 6;
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Location = new System.Drawing.Point(338, 164);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(99, 22);
+            this.txtDonGia.TabIndex = 5;
+            // 
+            // txtSL3
+            // 
+            this.txtSL3.Location = new System.Drawing.Point(127, 164);
+            this.txtSL3.Name = "txtSL3";
+            this.txtSL3.Size = new System.Drawing.Size(102, 22);
+            this.txtSL3.TabIndex = 4;
+            // 
+            // txtTenSP3
+            // 
+            this.txtTenSP3.Location = new System.Drawing.Point(127, 94);
+            this.txtTenSP3.Name = "txtTenSP3";
+            this.txtTenSP3.Size = new System.Drawing.Size(310, 22);
+            this.txtTenSP3.TabIndex = 3;
+            // 
+            // txtMaSP3
+            // 
+            this.txtMaSP3.Location = new System.Drawing.Point(127, 35);
+            this.txtMaSP3.Name = "txtMaSP3";
+            this.txtMaSP3.Size = new System.Drawing.Size(310, 22);
+            this.txtMaSP3.TabIndex = 2;
+            // 
+            // btXong3
+            // 
+            this.btXong3.Location = new System.Drawing.Point(412, 199);
+            this.btXong3.Name = "btXong3";
+            this.btXong3.Size = new System.Drawing.Size(97, 39);
+            this.btXong3.TabIndex = 1;
+            this.btXong3.Text = "xong ";
+            this.btXong3.UseVisualStyleBackColor = true;
+            this.btXong3.Click += new System.EventHandler(this.btXong3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
             // 
             // tabPage4
             // 
@@ -99,7 +346,7 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 28);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(829, 418);
+            this.tabPage4.Size = new System.Drawing.Size(1169, 688);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Quản lý khách hàng";
             // 
@@ -109,7 +356,7 @@
             this.tabPage5.Location = new System.Drawing.Point(4, 28);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(829, 418);
+            this.tabPage5.Size = new System.Drawing.Size(1169, 688);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Quản lý hóa đơn";
             // 
@@ -119,7 +366,7 @@
             this.tabPage6.Location = new System.Drawing.Point(4, 28);
             this.tabPage6.Name = "tabPage6";
             this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage6.Size = new System.Drawing.Size(829, 418);
+            this.tabPage6.Size = new System.Drawing.Size(1169, 688);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "Báo cáo thống kê";
             // 
@@ -127,11 +374,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(837, 450);
+            this.ClientSize = new System.Drawing.Size(1177, 720);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -145,5 +399,28 @@
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage5;
         private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTK3;
+        private System.Windows.Forms.Button btTK3;
+        private System.Windows.Forms.RadioButton rdoTH3;
+        private System.Windows.Forms.RadioButton rdoMH3;
+        private System.Windows.Forms.TextBox txtSLT3;
+        private System.Windows.Forms.TextBox txtDonGia;
+        private System.Windows.Forms.TextBox txtSL3;
+        private System.Windows.Forms.TextBox txtTenSP3;
+        private System.Windows.Forms.TextBox txtMaSP3;
+        private System.Windows.Forms.Button btXong3;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cl2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
--- XML_BTL/Form1.cs
+++ XML_BTL/Form1.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace XML_BTL
 {
@@ -15,6 +18,137 @@ namespace XML_BTL
         public Form1()
         {
             InitializeComponent();
+        }
+        XmlDocument doc = new XmlDocument();
+        XmlElement QLSP3;
+        string fileName = @"C:\Users\HACOM\source\repos\XML_BTL\XML_BTL\QLSP3.xml";
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+            HienThi3(dgv);
+        }
+        public void HienThi3(DataGridView dgv) {
+            doc.Load(fileName);
+            QLSP3 = doc.DocumentElement;
+            XmlNodeList ds = QLSP3.SelectNodes("SP3");
+            int sd = 0;
+            foreach (XmlNode node in ds)
+            {
+                dgv.Rows.Add();
+                dgv.Rows[sd].Cells[0].Value = node.SelectSingleNode("maSP3").InnerText;
+                dgv.Rows[sd].Cells[1].Value = node.SelectSingleNode("tenSP3").InnerText;
+                dgv.Rows[sd].Cells[2].Value = node.SelectSingleNode("SL3").InnerText;
+                dgv.Rows[sd].Cells[3].Value = node.SelectSingleNode("DG3").InnerText;
+                sd++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThi3(dgv);
+        }
+
+        private void dgv_MouseClick(object sender, MouseEventArgs e)
+        {
+           int i = dgv.CurrentCell.RowIndex;
+            txtMaSP3.Text = dgv.Rows[i].Cells[0].Value?.ToString() ?? "";
+            txtTenSP3.Text = dgv.Rows[i].Cells[1].Value?.ToString() ?? "";
+            txtSL3.Text = dgv.Rows[i].Cells[2].Value?.ToString() ?? "";
+            txtDonGia.Text = dgv.Rows[i].Cells[3].Value?.ToString() ?? "";
+        }
+
+        private void btTK3_Click(object sender, EventArgs e)
+        {
+            if (txtTK3.Text == "") MessageBox.Show("Vui lòng nhập thứ bạn muốn tìm");
+             else if(rdoMH3.Checked)
+            {
+                dgv.Rows.Clear();
+                XmlNode TK3 = QLSP3.SelectSingleNode("SP3[maSP3 ='" + txtTK3.Text.Trim().ToLower() + "']");
+                if (TK3 != null)
+                {
+                    // dgv.Rows.Add();//thêm một dòng mới
+                    //đưa dữ liệu vào dòng vừa tạo
+                    dgv.Rows[0].Cells[0].Value = TK3.SelectSingleNode("maSP3").InnerText;
+                    dgv.Rows[0].Cells[1].Value = TK3.SelectSingleNode("tenSP3").InnerText;
+                    dgv.Rows[0].Cells[2].Value = TK3.SelectSingleNode("SL3").InnerText;
+                    dgv.Rows[0].Cells[3].Value = TK3.SelectSingleNode("DG3").InnerText;
+                }
+                else { MessageBox.Show("không có thứ bạn muốn tìm "); }
+
+            }
+            else if (rdoTH3.Checked)
+            {
+                dgv.Rows.Clear();
+                string searchText = txtTK3.Text.Trim().ToLower(); // Chuyển về chữ thường
+
+                // Sử dụng XPath chính xác để tìm kiếm
+                XmlNode TK31 = QLSP3.SelectSingleNode($"SP3[translate(tenSP3, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz') = '{searchText}']");
+
+                if (TK31 != null)
+                {
+                    dgv.Rows.Add();
+                    dgv.Rows[0].Cells[0].Value = TK31.SelectSingleNode("maSP3").InnerText;
+                    dgv.Rows[0].Cells[1].Value = TK31.SelectSingleNode("tenSP3").InnerText;
+                    dgv.Rows[0].Cells[2].Value = TK31.SelectSingleNode("SL3").InnerText;
+                    dgv.Rows[0].Cells[3].Value = TK31.SelectSingleNode("DG3").InnerText;
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy sản phẩm bạn muốn tìm.");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Vui lòng chọn Mã hàng hoặc Tên Hàng");
+            }
+        }
+
+        private void btXong3_Click(object sender, EventArgs e)
+        {
+            doc.Load(fileName);// load tệp xml
+            QLSP3 = doc.DocumentElement;
+
+            string maSP = txtMaSP3.Text.Trim();
+            string soLuongThem = txtSLT3.Text.Trim();
+            XmlNode SPNode = QLSP3.SelectSingleNode($"//SP3[maSP3='{maSP}']");
+            if (SPNode != null)
+            {
+                // Nếu mã sản phẩm đã tồn tại, cộng số lượng mới vào số lượng hiện có
+                int soLuongHienCo = int.Parse(SPNode.SelectSingleNode("SL3").InnerText);
+                int soLuongMoi = int.Parse(soLuongThem);
+                int tongSoLuong = soLuongHienCo + soLuongMoi;
+                SPNode.SelectSingleNode("SL3").InnerText = tongSoLuong.ToString();
+                doc.Save(fileName);//lưu dữ liệu
+                HienThi3(dgv);
+            }
+            else
+            {
+                // Nếu mã sản phẩm chưa tồn tại, thêm sản phẩm mới vào tập tin XML
+                XmlNode SP3 = doc.CreateElement("SP3");
+                XmlElement maSP3 = doc.CreateElement("maSP3");
+                maSP3.InnerText = txtMaSP3.Text;//gán giá trị trên ô textbox txtMS cho node mã sách
+                SP3.AppendChild(maSP3);// gán node masach là node con của node sach
+
+                XmlElement tenSP3 = doc.CreateElement("tenSP3");// tạo 1 element node ten sach
+                tenSP3.InnerText = txtTenSP3.Text;// gán giá trị trên ô textbox txttenS cho node tensach
+                SP3.AppendChild(tenSP3);//gán node ténach là node con của node sach
+
+                XmlElement SL3 = doc.CreateElement("SL3");
+                SL3.InnerText = txtSL3.Text;
+                SP3.AppendChild(SL3);
+
+                XmlElement DG3 = doc.CreateElement("DG3");
+                DG3.InnerText = txtDonGia.Text;
+                SP3.AppendChild(DG3);
+
+                //sau khi tạo xong node sach, thì thêm sach vào gốc root
+                QLSP3.AppendChild(SP3);
+                doc.Save(fileName);//lưu dữ liệu
+                HienThi3(dgv);
+
+
+            }
         }
     }
 }
